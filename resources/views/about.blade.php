866306b9dad15bec24449b003aad6689c51aa3f2
unknown <fatursibolga@gmail.com>
2024-12-16 16:04:50 +0700

    PUSH pertama saya untuk project laravel kedalam github

---
+++ resources/views/about.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>About</title>
+</head>
+
+<body>
+  <h1>Ini adalah halaman about</h1>
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li>About</li>
+    <li><a href="/mahasiswa">Mashasiswa</a></li>
+    <li><a href="/profil">Profil</a></li>
+    </li>
+  </ul>
+
+</body>
+
+</html>
---
+++ resources/views/profil.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>profil</title>
+</head>
+
+<body>
+  <h1>Ini adalah halaman profil</h1>
+  <p> Nama saya adalah {{ $nama;}}
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/mahasiswa">Mahasiswa</a></li>
+      <li>Profil</li>
+      </li>
+    </ul>
+</body>
+
+</html>
