fe3401aae1e142fd14705b2c294396bc60ac8854
unknown <fatursibolga@gmail.com>
2024-12-17 16:36:03 +0700

    Membuat controller mahasiswacontroller

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,8 @@
 <?php
 
+use App\Http\Controllers\mahasiswaController;
 use Illuminate\Support\Facades\Route;
 /*
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-
-Route::get('about', function () {
-    return  view('about');
-});
-
-Route::get('mahasiswa', function () {
-    $npm = [123, 124, 125, 126];
-    $nama = ['Ipul', 'Saipul', 'Irul', 'Raul'];
-    $jumlah = count($npm);
-    return view('mahasiswa', compact('npm', 'jumlah', 'nama'));
-});
-
-Route::get('profil', function () {
-    $nama = 'arthu';
-    return view('profil', compact('nama'));
-});
-
-Route::get('array', function () {
-    for ($i = 1; $i <= 5; $i++) {
-        echo 'Hello' . $i . 'x<br>';
-    }
-});
+Route::get('mahasiswa', [mahasiswaController::class, 'index']);
