552c8b51d950ab3d66f99f30332b4a9e7ca05ad8
unknown <fatursibolga@gmail.com>
2024-12-17 17:16:12 +0700

    Membuat controller mahasiswaController menggunakan controller resource dengan membuat halaman mahasiswa dan create mahasiswa

--- app/Http/Controllers/mahasiswaController.php
+++ app/Http/Controllers/mahasiswaController.php
@@ -3,18 +3,65 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\Return_;
 
 class mahasiswaController extends Controller
 {
-    //index
-    function index()
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        return view('mahasiswa');
+        //Halaman Home Mahasiswa
+        return view('mahasiswa/mahasiswa');
     }
-    //edit
-    //tambah
-    //simpan tambah
-    //simpan edir
-    //detail
-    //hapus
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //Halaman Tambah Mahasiswa
+        return view('mahasiswa/create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //Simpan Tambah Mahasiswa
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //Halaman Detail
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //Halaman Edit Mahasiswa
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //Halaman Edit Mahasiswa
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //Hapus Mahasiswa
+    }
 }
--- resources/views/mahasiswa/mahasiswa.blade.php
+++ resources/views/mahasiswa/mahasiswa.blade.php
@@ -38,8 +38,17 @@
   <h1>ini adalah halaman mahasiswa</h1>
 
   <div class="row">
-    <div  class="col-sm-6">
+    <div class="col-sm-6">
       <h4>Tabel Mahasiswa</h4>
+    </div>
+    <div class="col-sm-6" style="text-align:right">
+      <a href="/mahasiswa/create" class="btn btn-info btn-sm">Tambah Mahasiswa</a>
+    </div>
+  </div>
+
+  <div class="row">
+    <div  class="col-sm-12">
+      
       <table class="table table-danger table-sm table-hover table-striped table-bordered text-center">
         <thead>
         <tr>
@@ -54,45 +63,6 @@
         </tbody>
       </table>
     </div>
-    <div class="col-sm-6">
-      <h4>Form Mahasiswa</h4>
-      <form action="" method="POST">
-        <div class="row">
-          <div class="col-sm-6">
-            <label for="">NIM</label>
-            <input type="number" name="nim" class="form-control" placeholder="Input Nim">
-          </div>
-          <div class="col-sm-6">
-            <label for="">Nama Mahasiswa</label>
-            <input type="text" name="nama mahasiswa" id="" class="form-control" placeholder="Masukkan Nama">
-          </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-6">
-            <label for=""> Tanggal Lahir</label>
-            <input type="date" name="tanggal lahir" id="" class="form-control">
-            </div>
-          <div class="col-sm-6">
-            <label for="">Jurusan</label>
-            <select name="jurusan" id="" class="form-control">
-              <option>Tekhnik Komputer</option>
-              <option>Sistem Informasi</option>
-              <option>Matematika</option>
-              <option>Kimia</option>
-              <option>Fisika Terapan</option>
-              <option>Ilmu Perpustakaan</option>
-              <option>Ilmu Komunikasi</option>
-            </select>
-          </div>  
-        </div>
-        <div class="row mt-2">
-          <div class="col-sm-12">
-            <button class="btn btn-primary" style="width: 100%" type="submit">Simpan</button>
-          </div>
-        </div>
-      </form>
-  </div>
-
 </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
